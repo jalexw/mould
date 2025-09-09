8b45c49f82db1afa257719f71ffa1b2ef5abf7c5
jalexw <jalexwhitman@gmail.com>
2025-09-09 00:20:44 -0300

    0.0.8 - flush out what .mouldconfig.json inputs field looks like more (add required property)

--- src/types/ITemplateConfig.ts
+++ src/types/ITemplateConfig.ts
@@ -1,14 +1,28 @@
 import { z } from "zod";
 
+const mouldInputTypes = ["text"] as const satisfies readonly string[];
+
+type MouldInputType = (typeof mouldInputTypes)[number];
+
 const mouldInputsItemDefinition = z
   .object({
     label: z.string(),
     id: z.string(),
     description: z.string().optional(),
+    required: z.boolean(),
+    type: z
+      .string()
+      .refine((str): str is MouldInputType =>
+        (
+          mouldInputTypes satisfies readonly string[] as readonly string[]
+        ).includes(str),
+      ),
   })
   .required({
     label: true,
     id: true,
+    required: true,
+    type: true,
   })
   .strict();
 
